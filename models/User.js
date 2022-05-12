316e7786f4e184fbf4b3b7889532d9e8505def74
MorningSol <98545619+MorningSol@users.noreply.github.com>
2022-05-12 16:23:42 -0400

    verify login and password

--- models/User.js
+++ models/User.js
@@ -3,7 +3,12 @@ const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 
 // create our User model
-class User extends Model {}
+class User extends Model {
+    //set up method to run on instance data (per user) to check password
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
 
 // define tbale columns and configuration
 User.init(
@@ -43,10 +48,7 @@ User.init(
                 // this means the password must be at least four characters long
                 len:[4]
             }
-        }
-         
-  
-       
+        }  
     },
 
     {
@@ -56,7 +58,7 @@ User.init(
                 newUserData.password = await bcrypt.hash(newUserData.password, 10);
                     return newUserData;
             },
-            
+
             // set up beforeUpdate lifecycle "hook" functionality
             async beforeUpdate(updatedUserData) {
                 updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
