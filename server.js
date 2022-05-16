b8bdc863e0d6f386cb0d4a2f9bd7417c7dcec1ee
MorningSol <98545619+MorningSol@users.noreply.github.com>
2022-05-16 13:03:10 -0400

    created homepage

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const path = require('path');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
