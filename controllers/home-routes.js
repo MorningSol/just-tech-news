7e54dfb2f62028dd3ff8f1841f564699e6fd898f
MorningSol <98545619+MorningSol@users.noreply.github.com>
2022-05-17 12:05:29 -0400

    added loginpage and logout

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,7 +4,9 @@ const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
-   Post.findAll({
+    console.log(req.session);
+
+    Post.findAll({
         attributes: [
             'id',
             'post_url',
@@ -36,6 +38,15 @@ router.get('/', (req, res) => {
         console.log(err);
         res.status(500).json(err);
     });
+});
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    
+    res.render('login');
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,7 +4,19 @@ const hbs = exphbs.create({});
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const session = require('express-session');
 
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -15,6 +27,7 @@ app.set('view engine', 'handlebars');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(session(sess));
 
 // turn on routes
 app.use(routes);
