5e5373b15c5507d21650705b9a812a8a6c883140
MorningSol <98545619+MorningSol@users.noreply.github.com>
2022-05-19 13:20:32 -0400

    finished app

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,13 +1,13 @@
 const router = require('express').Router();
 const { Comment } = require('../../models');
-
+const withAuth = require('../../utils/auth');
 
 router.get('/', (req, res) => {
 
 });
 
 
-router.post('/', (req, res) => {
+router.post('/', withAuth, (req, res) => {
     // check the session
     if (req.session) {
         Comment.create({
@@ -25,7 +25,7 @@ router.post('/', (req, res) => {
 });
 
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', withAuth, (req, res) => {
 
 });
 
